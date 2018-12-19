aab3fe6563bb49f46ccd34e06179691c2167c2b5
chefe <hi@chefe.io>
2018-12-19 10:12:37 +0100

    Adjust ownership demos for better presentation

--- code/ownership/src/02-string-normal.rs
+++ code/ownership/src/02-string-normal.rs
@@ -1,5 +1,6 @@
 fn main() {
     let s1 = String::from("hello");
     let s2 = s1;
-    println!("{}, world!", s1);
+    println!("{} from s1!", s1);
+    println!("{} from s2!", s2);
 }
--- code/ownership/src/07-single-mutable-ref.rs
+++ code/ownership/src/07-single-mutable-ref.rs
@@ -2,4 +2,6 @@ fn main() {
     let mut s = String::from("hello");
     let r1 = &mut s;
     let r2 = &mut s;
+    println!("{}", r1);
+    println!("{}", r2);
 }
